1b8d9709e57b8dc5358ee6d8bcdcc4efb490359f
sj <songjian@codeorder.cn>
2022-10-10 10:53:55 +0800

    完善包信息

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # vCard
 Python操作Android手机联系人的包。
-
-## 安装
-
-```sh
-pip install git+https://github.com/songjian/telegram-contacts.git
-```
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="telegram-contacts",
+    name="vCard",
     version="0.0.1",
     author="sj",
     author_email="songjian@codeorder.cn",
