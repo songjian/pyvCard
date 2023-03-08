8fda04b30d653ceb58130f04537b3728e760465d
sj <songjian@codeorder.cn>
2023-03-08 23:13:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python操作Android手机联系人的包。",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/songjian/vCard",
+    url="https://github.com/songjian/py-vCard",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
